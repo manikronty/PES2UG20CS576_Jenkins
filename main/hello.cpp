74beab85861710a576699661e220a44485481b24
manikronty <118479878+manikronty@users.noreply.github.com>
2023-02-17 19:21:03 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Manik,PES2UG20CS576"
+  cout << "I have successfully built and run\n";
+  return 0;
+}
