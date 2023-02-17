9338ada459d901bf1cf0489df3a006d3cec8b89f
manikronty <118479878+manikronty@users.noreply.github.com>
2023-02-17 20:01:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Manik, PES2UG20CS576";
   cout << "I have successfully built and run\n";
   return 0;
 }
